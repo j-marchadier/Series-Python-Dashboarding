3b7f19be46422c55664e7948c64229b3f04b3ecf
julien <marchadier.julien@gmail.com>
2021-10-07 12:50:29 +0200

    apres le diner

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 from numpy.lib.arraysetops import isin
 import pandas as pd
 import numpy as np
-import kaggle
+from pandas.core.frame import DataFrame
+#import kaggle
 
 pd.options.mode.chained_assignment = None
 
@@ -34,13 +35,33 @@ data.rename(columns={"release_date" : "release_year", "netflix_date" : "netflix_
 
 ########### Separe Country_availability ###########
 #fillna() <- remplacer les cases na
+#Création dataset about country
 data_country_availability = data[["title","release_year","country_availability"]]
 data_country_availability["country_availability"] = data["country_availability"][~data["country_availability"].isna()].map(lambda row : row.split(","))
+
+#Looking for all genre available
 all_country_availability =data_country_availability["country_availability"][data_country_availability["country_availability"].str.len() ==36].iloc[0]
 
+#création of col for each countries find
 for country in all_country_availability :
     data_country_availability[country] = np.where(data["country_availability"].str.contains(country), True, False)
 
-#data_country_availability = data_country_availability.drop(["country_availability"])
 data_country_availability.drop(["country_availability"],axis =1,inplace=True)
-print(data_country_availability.columns)
+
+
+########### Separe Genre ###########
+#Création dataset about genre
+data_genre_availability= data[["title","release_year","genre"]]
+data_genre_availability["genre"] = data["genre"][~data["genre"].isna()].map( lambda row : row.split(", "))    
+
+#Looking for all genre available
+all_genre_availability= []
+data_genre_availability["genre"][~data_genre_availability["genre"].isna()].map( 
+    lambda  row : [all_genre_availability.append(genre1) for genre1 in row])
+all_genre_availability=np.unique(all_genre_availability)
+
+#Création of col for each countries find
+for genre in all_genre_availability :
+    data_genre_availability[genre] = np.where(data["genre"].str.contains(genre,na=False), True, False)
+
+data_genre_availability.drop(["genre"],axis =1,inplace=True)
